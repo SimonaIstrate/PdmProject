b85d926789573ecf36240e7c76800443fa7892e4
SimonaIstrate <simonaistrate.3112@gmail.com>
2019-10-22 19:55:23 +0300

    myCommit

---
+++ PdmProject/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PdmProject
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
